e1c44457e22fd36e3b8cf0e7f1aef54c1c41db01
Rajeev Ranjan <rajeev@infrrdad.local>
2020-02-14 16:02:58 +0530

    button added

--- App.tsx
+++ App.tsx
@@ -1,36 +1,43 @@
-import React, { Component } from 'react';
+import React, * as react from 'react';
 import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 
-export default class HelloWorldApp extends Component {
-  _onPressButton(){
+export default class HelloWorldApp extends react.Component {
+  _onPressButton() {
     alert('you tapped the button!')
   }
   render() {
     return (
       <View style={styles.container}>
-        <FlatList data={[{key: 'Devin'},
-            {key: 'Dan'},
-            {key: 'Dominic'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'first'},
-            {key: 'second'},
-            {key: 'third'},
-            {key: 'fourth'},
-            {key: 'fifth'},
-            {key: 'sixth'},
-            {key: 'seventh'},
-            {key: 'eight'},
-            {key: 'ninth'},
-            {key: 'tenth'}, 
-            {key: 'eleventh'},
-            {key: 'tweleveth'},
-            {key: 'thirteenth'}]} 
-            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
+
+        <View style={styles.flatList}>
+          <FlatList data={[{ key: 'Devin' },
+          { key: 'Dan' },
+          { key: 'Dominic' },
+          { key: 'Jackson' },
+          { key: 'James' },
+          { key: 'Joel' },
+          { key: 'John' },
+          { key: 'Jillian' },
+          { key: 'Jimmy' },
+          { key: 'first' },
+          { key: 'second' },
+          { key: 'third' },
+          { key: 'fourth' },
+          { key: 'fifth' },
+          { key: 'sixth' },
+          { key: 'seventh' },
+          { key: 'eight' },
+          { key: 'ninth' },
+          { key: 'tenth' },
+          { key: 'eleventh' },
+          { key: 'tweleveth' },
+          { key: 'thirteenth' }]}
+            renderItem={({ item }) => <Text style={styles.flatListitem}>{item.key}</Text>} />
+        </View>
+
+        <View style={styles.formContainerLayout}>
+          <Button onPress={this._onPressButton} title="Press Me"/>
+        </View>
       </View>
     );
   }
@@ -38,14 +45,21 @@ export default class HelloWorldApp extends Component {
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22,
-   justifyContent: "center",
-   alignItems: "center"
+    flexDirection: "column",
+    paddingTop: 22,
+    justifyContent: "center",
+    alignItems: "center"
   },
-  item: {
+  flatList: {
+    flexGrow: 1,
+    height: "20%"
+  },
+  flatListitem: {
     padding: 10,
     fontSize: 18,
     height: 44,
   },
+  formContainerLayout: {
+    flex: 2
+  }
 })
