0d5e7ea32eb523fdc64dc4e1bab3545adabb33c8
Rajeev Ranjan <rajeev@infrrdad.local>
2020-02-14 15:32:07 +0530

    basic list view added

--- App.tsx
+++ App.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>this is the test project</Text>
-    </View>
-  );
+export default class HelloWorldApp extends Component {
+  _onPressButton(){
+    alert('you tapped the button!')
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList data={[{key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'}]} 
+            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
   },
-});
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
